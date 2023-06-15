e885cbc602c66e2d1cd6fb616561f4defe816070
Jvalant <elitesolutions.jp@gmail.com>
2023-06-15 14:41:14 +0530

    take user input for topic of the day, add email subject

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import requests
 from send_email import send_email
 
+topic = input("Please enter topic for the day")
 api_key = "aec9b642525d4c9caee716b80e3b88c8"
-url = "https://newsapi.org/v2/top-headlines?" \
-      "country=in&category=business&apiKey=" \
-      "aec9b642525d4c9caee716b80e3b88c8"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic} " \
+      "&from=2023-05-15" \
+      "&sortBy=publishedAt" \
+      "&apiKey=aec9b642525d4c9caee716b80e3b88c8" \
+      "&language=en"
+
 request = requests.get(url)
 content = request.json()
 
-for article in content["articles"]:
+for article in content["articles"][:20]:
     try:
         print(article['url'])
         print(article['description'])
-        message = article['title'].title() + "\n" + article['description']
+        message = "Subject: Today's News" + "\n" + \
+                  article['title'].title() + "\n" + \
+                  article['description'] + "\n" + \
+                  article["url"] + 2*"\n"
         message = message.encode("utf-8")
-        send_email(message)
     except TypeError:
         continue
+    send_email(message)
+
